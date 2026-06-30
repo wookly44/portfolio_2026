9365f6de83e0948c518fae21b1e902f413d239cd
wookly44 <goldweek49@gmail.com>
2026-07-01 06:28:43 +0900

    chore: fix GitHub Pages deploy path

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/portfolio_2026/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
